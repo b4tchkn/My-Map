53855d98f01b523135b9b19379383fd836efdfe7
Kakeru Nakabachi <b1015146@fun.ac.jp>
2018-03-14 11:39:04 +0900

    keyword to location

--- MyMap/ViewController.swift
+++ MyMap/ViewController.swift
@@ -9,20 +9,54 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        inputText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBOutlet weak var inputText: UITextField!
-    
     @IBOutlet weak var dispMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを閉じる
+        textField.resignFirstResponder()
+        
+        // 入力された文字取り出す
+        if let searchKey = textField.text {
+            print(searchKey)
+            
+            // CLGeocoderインスタンスを取得
+            let geocoder = CLGeocoder()
+            
+            // 入力された文字から位置情報を取得
+            geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks, error) in
+                
+                // 位置情報が存在する場合はunwrapPlacemarksに取り出す
+                if let unwrapPlacemarks = placemarks {
+                    
+                    // 1件目の情報を取り出す
+                    if let firstPlacemark = unwrapPlacemarks.first {
+                        
+                        // 位置情報を取り出す
+                        if let location = firstPlacemark.location {
+                            
+                            // 位置情報から緯度経度をtargetCoordinateに取り出す
+                            let targetCoordinate = location.coordinate
+                            
+                            print(targetCoordinate)
+                        }
+                    }
+                }
+            })
+        }
+        
+        return true
+    }
 }
 
